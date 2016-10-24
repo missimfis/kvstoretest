299216fca04eaa4d793b642be465e640390cabcb
missimfis <missimfis@localhost.localdomain>
2016-10-24 11:00:55 +0200

    get the bug out of the system

--- registry.py
+++ registry.py
@@ -58,4 +58,4 @@ def application(environ, start_response):
     response_headers = [('Content-Type', ctype), ('Content-Length', str(len(response_body)))]
 
     start_response(status, response_headers)
-return [response_body]
+    return [response_body]
