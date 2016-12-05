c61ec9d6809ae9d491faf5609700352b4820f304
fischmis <fischmis@students.zhaw.ch>
2016-12-05 10:33:03 +0100

    fixed intentions

--- store.py
+++ store.py
@@ -10,23 +10,23 @@ def application(environ, start_response):
 
     ctype = 'text/plain'
     status = '200 OK'
-data = {  
-    "_class": "Storage",
-    "account": "fischmis",
-    "unit": "pair",
-    "usage": 1
-}
+    data = {  
+        "_class": "Storage",
+        "account": "fischmis",
+        "unit": "pair",
+        "usage": 1
+    }
 
 
     if environ['PATH_INFO'] == '/store':
       response_body = ",".join([x + "=" + y for x, y in store.items()])
-r = requests.post('http://160.85.4.251:4567/data', json.dumps(data))
+      r = requests.post('http://160.85.4.251:4567/data', json.dumps(data))
     elif environ['PATH_INFO'].startswith('/store/'):
       kvpair = environ['PATH_INFO'][7:]
       k, v = kvpair.split("=")
       store[k] = v
       response_body = "Key {:s} added with value {:s}.".format(k, v)
-r = requests.post('http://160.85.4.251:4567/data', json.dumps(data))
+      r = requests.post('http://160.85.4.251:4567/data', json.dumps(data))
     else:
       response_body = "Error: invalid request."
       status = '599 LOST'
